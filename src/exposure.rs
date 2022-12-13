bd8c3a4b053d20a09545ddde9e206befeaffffed
runeblaze <runeblaze@excite.co.jp>
2022-12-12 23:51:16 -0500

    moved out unused structs

--- src/exposure.rs
+++ src/exposure.rs
@@ -158,7 +158,7 @@ pub fn post_read_singleton(
     Ok(df)
 }
 
-pub fn read_assignment_series(
+pub fn read_membership_series(
     g: &Graph,
     nodes: &Series,
     cids: &Series,
@@ -204,7 +204,7 @@ pub fn read_assignment_series(
     Ok(df)
 }
 
-pub fn read_assignment_file(
+pub fn read_membership_file(
     g: &Graph,
     filepath: &str,
     sep: u8,
@@ -216,27 +216,27 @@ pub fn read_assignment_file(
         .finish()?;
     let nid = df.column("column_1")?;
     let cid = df.column("column_2")?;
-    read_assignment_series(g, nid, cid, mode)
+    read_membership_series(g, nid, cid, mode)
 }
 
 #[pyfunction(
-    name = "read_assignment",
+    name = "read_membership",
     mode = "SingletonMode::AutoPopulate",
-    sep = "b'\\t'"
+    sep = "'\\t'"
 )]
-pub fn py_read_assignment_file(
+pub fn py_read_membership_file(
     g: &Graph,
     filepath: &str,
-    sep: u8,
+    sep: char,
     mode: SingletonMode,
 ) -> PyResult<PyObject> {
-    let mut df = read_assignment_file(g, filepath, sep, mode).unwrap();
+    let mut df = read_membership_file(g, filepath, sep as u8, mode).unwrap();
     let translated = translate_df(&mut df)?;
     Ok(translated)
 }
 
-#[pyfunction(name = "read_assignment_series", mode = "SingletonMode::AutoPopulate")]
-pub fn py_from_assignments(
+#[pyfunction(name = "read_membership_series", mode = "SingletonMode::AutoPopulate")]
+pub fn py_from_memberships(
     g: &Graph,
     nodes: &PyAny,
     cids: &PyAny,
@@ -244,7 +244,7 @@ pub fn py_from_assignments(
 ) -> PyResult<PyObject> {
     let nodes = ffi::py_series_to_rust_series(nodes)?;
     let cids = ffi::py_series_to_rust_series(cids)?;
-    let mut df = read_assignment_series(g, &nodes, &cids, mode).unwrap();
+    let mut df = read_membership_series(g, &nodes, &cids, mode).unwrap();
     translate_df(&mut df)
 }
 
@@ -297,9 +297,6 @@ pub trait ClusDataFrame {
     fn modularity(&self, graph: &Graph, resolution: f64) -> anyhow::Result<Series>;
     fn cpm(&self, resolution: f64) -> anyhow::Result<Series>;
     fn covered_num_nodes(&self) -> anyhow::Result<u32>;
-    fn edges(&self, graph: &DefaultGraph) -> anyhow::Result<Series>;
-    fn covered_edges(&self, graph: &DefaultGraph) -> anyhow::Result<Series>;
-    fn can_overlap(&self, graph: &DefaultGraph) -> bool;
 }
 
 impl ClusDataFrame for DataFrame {
@@ -343,18 +340,6 @@ impl ClusDataFrame for DataFrame {
                 let n = self.column("n")?.u32()?;
                 Ok(n.into_iter().map(|n| n.unwrap()).sum())
             })
-    }
-
-    fn edges(&self, _graph: &DefaultGraph) -> anyhow::Result<Series> {
-        todo!()
-    }
-
-    fn covered_edges(&self, _graph: &DefaultGraph) -> anyhow::Result<Series> {
-        todo!()
-    }
-
-    fn can_overlap(&self, _graph: &DefaultGraph) -> bool {
-        todo!()
     }
 }
 
@@ -498,253 +483,6 @@ impl Graph {
     }
 }
 
-#[pyclass]
-pub struct ClusterSkeleton {
-    #[pyo3(get)]
-    n: u64,
-    #[pyo3(get)]
-    m: u64,
-    #[pyo3(get)]
-    c: u64,
-    #[pyo3(get)]
-    mcd: u64,
-    #[pyo3(get)]
-    vol: u64,
-}
-
-#[pymethods]
-impl ClusterSkeleton {
-    pub fn __str__(&self) -> PyResult<String> {
-        Ok(format!(
-            "ClusterSkeleton(n={}, m={}, c={})",
-            self.n, self.m, self.c,
-        ))
-    }
-}
-
-impl From<RichCluster> for ClusterSkeleton {
-    fn from(cluster: RichCluster) -> Self {
-        ClusterSkeleton {
-            n: cluster.n,
-            m: cluster.m,
-            c: cluster.c,
-            mcd: cluster.mcd,
-            vol: cluster.vol,
-        }
-    }
-}
-
-impl ClusterSkeleton {
-    fn from_cluster(cluster: &RichCluster) -> Self {
-        ClusterSkeleton {
-            n: cluster.n,
-            m: cluster.m,
-            c: cluster.c,
-            mcd: cluster.mcd,
-            vol: cluster.vol,
-        }
-    }
-}
-
-#[pyclass]
-pub struct Clustering {
-    data: Arc<RichClustering<true>>,
-}
-
-#[pyclass]
-pub struct ClusteringSubset {
-    data: ClusteringHandle<true>,
-}
-
-#[pymethods]
-impl Clustering {
-    #[new]
-    #[args(py_kwargs = "**")]
-    fn new(
-        py: Python,
-        graph: &Graph,
-        filepath: &str,
-        py_kwargs: Option<&PyDict>,
-    ) -> PyResult<Self> {
-        let mut source = ClusteringSource::Unknown;
-        if let Some(kwargs) = py_kwargs {
-            if let Some(cpm_resolution) = kwargs.get_item("cpm") {
-                source = ClusteringSource::Cpm(cpm_resolution.extract()?);
-            }
-        }
-        let raw_data = py.allow_threads(move || {
-            let mut clus =
-                RichClustering::<true>::pack_from_file(graph.data.clone(), filepath).unwrap();
-            clus.source = source;
-            clus
-        });
-        Ok(Clustering {
-            data: Arc::new(raw_data),
-        })
-    }
-
-    fn __getitem__(&self, ids: &PyList) -> PyResult<ClusteringSubset> {
-        let ids: Vec<u32> = ids.extract()?;
-        let data = ClusteringSubset {
-            data: ClusteringHandle::new(self.data.clone(), ids.into_iter().collect(), false),
-        };
-        Ok(data)
-    }
-
-    fn filter(&self, f: &PyAny) -> PyResult<ClusteringSubset> {
-        let v = self
-            .data
-            .clusters
-            .iter()
-            .filter(|(_k, v)| {
-                f.call((ClusterSkeleton::from_cluster(v),), None)
-                    .unwrap()
-                    .extract()
-                    .unwrap()
-            })
-            .map(|(k, _v)| k)
-            .copied()
-            .collect();
-        let has_singletons = f
-            .call(
-                (ClusterSkeleton {
-                    n: 1,
-                    m: 0,
-                    c: 0,
-                    mcd: 0,
-                    vol: 0,
-                },),
-                None,
-            )
-            .unwrap()
-            .extract()
-            .unwrap();
-        Ok(ClusteringSubset {
-            data: ClusteringHandle::new(self.data.clone(), v, has_singletons),
-        })
-    }
-
-    pub fn __str__(&self) -> PyResult<String> {
-        Ok(format!(
-            "Clustering(covered_nodes={}, size={})",
-            self.data.cover.len(),
-            self.data.clusters.len(),
-        ))
-    }
-
-    pub fn size(&self) -> usize {
-        self.data.clusters.len()
-    }
-}
-
-#[pyclass(name = "ClusteringStats")]
-pub struct StatsWrapper {
-    #[pyo3(get)]
-    num_clusters: u32,
-    #[pyo3(get)]
-    covered_nodes: u32,
-    #[pyo3(get)]
-    covered_edges: u64,
-    #[pyo3(get)]
-    total_nodes: u32,
-    #[pyo3(get)]
-    total_edges: u64,
-    #[pyo3(get)]
-    distributions: HashMap<String, SummarizedDistributionWrapper>,
-}
-
-impl StatsWrapper {
-    pub fn from_graph_stats(graph_stats: GraphStats) -> Self {
-        StatsWrapper {
-            num_clusters: graph_stats.num_clusters,
-            covered_nodes: graph_stats.covered_nodes,
-            covered_edges: graph_stats.covered_edges,
-            total_nodes: graph_stats.total_nodes,
-            total_edges: graph_stats.total_edges,
-            distributions: graph_stats
-                .statistics
-                .into_iter()
-                .map(|(k, v)| {
-                    (
-                        k.to_string().to_lowercase(),
-                        SummarizedDistributionWrapper::new(v),
-                    )
-                })
-                .collect(),
-        }
-    }
-}
-
-#[pyclass(name = "SummarizedDistribution")]
-#[derive(Debug, Clone)]
-pub struct SummarizedDistributionWrapper {
-    data: aocluster::belinda::SummarizedDistribution,
-}
-
-impl SummarizedDistributionWrapper {
-    fn new(data: aocluster::belinda::SummarizedDistribution) -> Self {
-        SummarizedDistributionWrapper { data }
-    }
-}
-
-#[pymethods]
-impl SummarizedDistributionWrapper {
-    #[getter]
-    pub fn percentiles(&self) -> Vec<f64> {
-        self.data.percentiles.to_vec()
-    }
-
-    #[getter]
-    pub fn minimum(&self) -> f64 {
-        self.data.minimum()
-    }
-
-    #[getter]
-    pub fn maximum(&self) -> f64 {
-        self.data.maximum()
-    }
-
-    #[getter]
-    pub fn median(&self) -> f64 {
-        self.data.median()
-    }
-}
-
-// pub fn union_bitmaps<E: AsRef<[Expr]>>(exprs: E) -> Expr {
-//     let exprs = exprs.as_ref().to_vec();
-
-//     let function = SpecialEq::new(Arc::new(move |series: &mut [Series]| {
-//         let mut s_iter = series.iter();
-
-//         match s_iter.next() {
-//             Some(acc) => {
-//                 let mut acc = acc.clone();
-//                 let bitmaps = iter_roaring(&acc)
-//                     .map(|it| it.try_into().unwrap())
-//                     .collect::<Vec<RoaringBitmap>>();
-//                 let series = build_series_from_bitmap(vec![bitmaps.union()]);
-//                 Ok(series)
-//             }
-//             None => Err(PolarsError::ComputeError(
-//                 "Reduce did not have any expressions to fold".into(),
-//             )),
-//         }
-//     }) as Arc<dyn SeriesUdf>);
-
-//     Expr::AnonymousFunction {
-//         input: exprs,
-//         function,
-//         output_type: GetOutput::super_type(),
-//         options: FunctionOptions {
-//             collect_groups: ApplyOptions::ApplyGroups,
-//             input_wildcard_expansion: true,
-//             auto_explode: true,
-//             fmt_str: "reduce",
-//             ..Default::default()
-//         },
-//     }
-// }
-
 pub fn rust_label_cc(g: &Graph, series: &Series) -> anyhow::Result<Series> {
     let labels = &g.get_cc_labels().labels;
     let g = &g.data.graph;
@@ -815,12 +553,6 @@ fn edgeset(g: &EnrichedGraph, bm: &RoaringBitmap) -> RoaringTreemap {
     tm
 }
 
-// pub fn rust_edgeset(series: &Series) -> Series {
-//     iter_roaring(series)
-//         .map(|bitmap| bitmap.len() as u32)
-//         .collect()
-// }
-
 #[pyfunction(name = "popcnt")]
 pub fn py_popcnt(series: &PyAny) -> PyResult<PyObject> {
     let series = ffi::py_series_to_rust_series(series)?;
@@ -856,107 +588,3 @@ pub fn py_nodeset_to_list(g: &Graph, series: &PyAny) -> PyResult<PyObject> {
     ffi::rust_series_to_py_series(&out)
 }
 
-#[pymethods]
-impl ClusteringSubset {
-    fn compute_statistics(&self, py: Python) -> StatsWrapper {
-        py.allow_threads(move || {
-            let stats = self.data.stats();
-            StatsWrapper::from_graph_stats(stats)
-        })
-    }
-
-    fn __getitem__(&self, key: u32) -> PyResult<ClusterSkeleton> {
-        let clus = &self.data.clustering;
-        if let Some(cluster) = clus.clusters.get(&key) {
-            Ok(ClusterSkeleton::from_cluster(cluster))
-        } else {
-            Err(PyErr::new::<pyo3::exceptions::PyKeyError, _>(
-                "Cluster not found",
-            ))
-        }
-    }
-
-    fn to_df(&self) -> PyResult<PyObject> {
-        let in_scope_clusters = self
-            .data
-            .cluster_ids
-            .iter()
-            .map(|it| &self.data.clustering.clusters[&it])
-            .collect::<Vec<_>>();
-        let mcd = in_scope_clusters
-            .iter()
-            .map(|it| it.mcd)
-            .collect::<Vec<_>>();
-        let mut df = df!("mcd" => mcd).unwrap();
-        translate_df(&mut df)
-    }
-
-    fn keys(&self) -> Vec<u32> {
-        self.data.cluster_ids.iter().collect()
-    }
-
-    fn size(&self) -> u64 {
-        self.data.cluster_ids.len()
-    }
-
-    fn compute_size_diff(&self, rhs: &Clustering) -> (u32, SummarizedDistributionWrapper) {
-        let (diff, dist) = self.data.size_diff(rhs.data.as_ref());
-        (diff, SummarizedDistributionWrapper::new(dist))
-    }
-
-    #[getter]
-    fn cluster_sizes(&self) -> Vec<u32> {
-        let d = &self.data;
-        d.cluster_ids
-            .iter()
-            .map(|k| d.clustering.clusters.get(&k).unwrap().nodes.len() as u32)
-            .collect()
-    }
-
-    #[getter]
-    fn node_coverage(&self) -> f64 {
-        self.data.get_covered_nodes() as f64 / self.data.graph.graph.n() as f64
-    }
-
-    #[getter]
-    fn num_singletons(&self) -> u32 {
-        if self.data.has_singletons {
-            self.data.clustering.singleton_clusters.len() as u32
-        } else {
-            0
-        }
-    }
-
-    fn node_multiplicities(&self) -> Vec<u32> {
-        let raw_mult = &self.data.node_multiplicity;
-        let mut mults: Vec<_> = self
-            .data
-            .covered_nodes
-            .iter()
-            .map(|n| raw_mult[n as usize])
-            .collect();
-        if self.data.has_singletons {
-            mults.extend((0..self.num_singletons()).map(|_| 1));
-        }
-        mults
-    }
-
-    #[getter]
-    fn node_multiplicities_dist(&self) -> SummarizedDistributionWrapper {
-        SummarizedDistributionWrapper::new(
-            self.node_multiplicities()
-                .into_iter()
-                .map(|it| it as f64)
-                .collect(),
-        )
-    }
-
-    fn __str__(&self) -> PyResult<String> {
-        Ok(format!(
-            "ClusteringSubset(size={}, node_coverage={:.1}%, is_overlapping={})",
-            self.data.cluster_ids.len(),
-            self.node_coverage() * 100.0,
-            self.data.is_overlapping
-        ))
-    }
-}
